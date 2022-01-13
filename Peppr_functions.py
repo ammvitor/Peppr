1b12b1d2e85524b6f87ccb8f5542399570e84e89
DozeDuck <s.xu24@ncl.ac.uk>
2022-01-13 15:57:41 +0000

    Deglobaling variables

--- Peppr_functions.py
+++ Peppr_functions.py
@@ -4,14 +4,12 @@ Spyder Editor
 
 This is a temporary script file.
 """
-from math import sqrt
 import sys
 import subprocess
 import os
 import numpy as np
 import getopt
 from Bio.PDB import PDBParser, PDBIO, Chain, Residue
-import matplotlib.pyplot as plt
 #you need to have biopython and autodock vina crankpep - export ADCPHOME where you installed it
 
 
@@ -62,9 +60,7 @@ def set_sequence():
     #finds the index of the X in the inpout sequence
     result = input_seq.find("X")       # Should be used to find the index number of "X", so here result is a number.
     global aminoacids                  # # Call aminoacids as a global variable for the using in next function
-    #aminoacids =["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"]
-    aminoacids =["A","I","L","M","F","W","Y","V","C","G","P","S","T","N","Q","R","H","D","E","K"]
-    #aminoacids =["A","I","L"]
+    aminoacids =["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"]
 
     #runs crankpep for each of the different aminoacids - first , it craets the 20 sequences to be tested
     for res in aminoacids:
@@ -75,22 +71,57 @@ def set_sequence():
     global residues                    # Define residues as global variable, originally named sequences
     residues = sequences               # Give sequences content to the variable-"residues"
 
+def structure_correct():
+    #calls biopdb to fix the broken pdb results for the top1 poses per sequence
+    io = PDBIO()
+    #fix it by loading to biopdb and printing 
+    total_number_of_output=["1","2","3","4","5","6","7","8","9","10"]
+    for rank in total_number_of_output:
+        try:
+            pdb = PDBParser(QUIET=True).get_structure("UGLY", outputname_ADCP+"_ranked_"+rank+".pdb")
+            io.set_structure(pdb)        
+            io.save(outputname_ADCP+"_ranked_"+rank+"_corrected.pdb")
+            #reads the output files with the energy to rank poses
+        except:
+            pass
+
+    
+def extract_score():
+    file = open(path_to_output_file,'r')                                        # open the file.txt in a read mode
+    Lines = file.readlines()                                                    # output the content in file.txt line by line into "Lines"
+    count =0
+    #skips the header of the file 
+    for line in Lines:
+        if line[0] != "-":
+            count = count + 1                                                   # find the best score line's previous line number
+        else:
+            break
+    #takes the first line - lowest energy and rank per residue
+    stripped_line = Lines[count+1].strip()                                      # count+1 means to locate to the line with best scores; .strip() means only extract this line
+    line_list = stripped_line.split()                                           # split() means to treat the extracted line into pices and save as elements in "line_list"
+    list_of_lists.append([])                                                    # vector <vector>
+    list_of_lists[len(list_of_lists)-1].append(aminoacids[residues.index(seq)]) # append the point changed residue name into the first element in list_of_lists
+    list_of_lists[len(list_of_lists)-1].append(seq)                             # append the changed peptide sequence into the second element in list_of_lists
+    list_of_lists[len(list_of_lists)-1].append(line_list[1])                    # append the best scores into the third element in list_of_lists
+        
+
 def adcp_run():
     global list_of_lists 
-    global CA_coordinates_final
-    CA_coordinates_final=[]
     list_of_lists=[]
     #run crankpep for all 20 sequences
+    global seq                                                                  # for global Structure Correct
     for seq in residues:
+
         print("Sequence tested now is: " + seq)
         #creates a varaible to output the files inside different folders
+        global outputname_ADCP                                                  # for global Structure Correct
         outputname_ADCP=seq+"/"+seq
         #creates a folder for each of the sequences
         subprocess.Popen(["mkdir",seq]).communicate()
+        global path_to_output_file                                              # for global Structure Correct
         path_to_output_file =outputname_ADCP+".txt"
         myoutput = open(path_to_output_file,'w+') # equal to open seq.txt then write down
         # the path to adcp should be fixed and sorted with a enviromental variable such as ADCPHOME
-        tmpname = "tmp_"+seq
         p = subprocess.Popen([adcphome,
                               "-t",input_receptor,"-s",
                               ''.join(new),  # Question: is this "new" = to the previous function's "new"?
@@ -105,80 +136,22 @@ def adcp_run():
                               "-c",
                               cores,
                               "-O"],stdout=myoutput).communicate()
-        subprocess.Popen(["rm" , "-r" ,  tmpname]).communicate()    #delete tmp folders 
-        def structure_correct():
-            CA_XYZ=[]
-            #calls biopdb to fix the broken pdb results for the top1 poses per sequence
-            io = PDBIO()
-            #fix it by loading to biopdb and printing
-            pdb = PDBParser(QUIET=True).get_structure("UGLY", outputname_ADCP+"_ranked_1.pdb")
-            io.set_structure(pdb)        
-            io.save(outputname_ADCP+"_ranked_1_corrected.pdb")
-            chains = list(pdb[0].get_chains())
-            residue = list(chains[0].get_residues())
-            for i in range(len(residue)):
-                atoms = list(residue[i].get_atoms())
-                for k in range(len(atoms)):
-                    if atoms[k].get_name()=="CA":
-                        XYZ=[]
-                        XYZ.append(atoms[k].get_vector()[0])
-                        XYZ.append(atoms[k].get_vector()[1])
-                        XYZ.append(atoms[k].get_vector()[2])
-                        CA_XYZ.append(XYZ)
-            #reads the output files with the energy to rank poses
-            return CA_XYZ
-        
-        CA_coordinates_final.append(structure_correct())
-        
-        def extract_score():
-            file = open(path_to_output_file,'r')                                        # open the file.txt in a read mode
-            Lines = file.readlines()                                                    # output the content in file.txt line by line into "Lines"
-            count =0
-            #skips the header of the file 
-            for line in Lines:
-                if line[0] != "-":
-                    count = count + 1                                                   # find the best score line's previous line number
-                else:
-                    break
-            #takes the first line - lowest energy and rank per residue
-            stripped_line = Lines[count+1].strip()                                      # count+1 means to locate to the line with best scores; .strip() means only extract this line
-            line_list = stripped_line.split()                                           # split() means to treat the extracted line into pices and save as elements in "line_list"
-            list_of_lists.append([])                                                    # vector <vector>
-            list_of_lists[len(list_of_lists)-1].append(aminoacids[residues.index(seq)]) # append the point changed residue name into the first element in list_of_lists
-            list_of_lists[len(list_of_lists)-1].append(seq)                             # append the changed peptide sequence into the second element in list_of_lists
-            list_of_lists[len(list_of_lists)-1].append(float(line_list[1]))                   # append the best scores into the third element in list_of_lists
-        
+        structure_correct()
         extract_score()
+            
+        
 
-def cross_rmsdmatrix():
-    CROSS_RMSD = []
-    for peptide_i in CA_coordinates_final:
-       RMSD_peptidei= []
-       for peptide_k in CA_coordinates_final:
-           RMSD = 0
-           for CA in range(len(CA_coordinates_final[0])):
-               RMSD = RMSD + sqrt((((peptide_i[CA][0] - peptide_k[CA][0])**2)+((peptide_i[CA][1] - peptide_k[CA][1])**2)+((peptide_i[CA][2] - peptide_k[CA][2])**2))/3)
-           RMSD_peptidei.append(RMSD/len(CA_coordinates_final[0]))
-       CROSS_RMSD.append(RMSD_peptidei)
-    fig, ax = plt.subplots()
-    plt.imshow(CROSS_RMSD, cmap='hot', interpolation='nearest')
-    ax.set_xticklabels(aminoacids)
-    ax.set_yticklabels(aminoacids)
-    ax.tick_params(top=True, bottom=False,
-                   labeltop=True, labelbottom=False)
-    ax.set_xticks(np.arange(len(aminoacids)))
-    ax.set_yticks(np.arange(len(aminoacids)))
 
-    plt.savefig('corssrmsd.png')
-    
-    
-def sort_list():
+
+def sort_list(): # positive numbers should be fixed
     #sort the list of 20 residues
-    sorted_multi_list = sorted(list_of_lists, reverse=False,key=lambda x: x[2])
+    sorted_multi_list = sorted(list_of_lists, reverse=True,key=lambda x: x[2])
     sorted_multi_array = np.array(sorted_multi_list)
+    # print(sorted_multi_array)
+    print("Results:")
     #prints in the screen the sorted list of 20 residues 
     for peptide in sorted_multi_list:
-        print(peptide[0]," ", peptide[1], " ", peptide[2] , " kcal/mol")
-
+        print("Score ", peptide[0]," ", peptide[1], " ", peptide[2] + " kcal/mol")
+# print("Congratulations! It's finished!")
 
 # peppr_sup() This is for testing whether def is working.
